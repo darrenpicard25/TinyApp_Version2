82307f081e197642f6deb1b9f471a2f07a03bb18
Darren Picard <darrenpicard25@gmail.com>
2019-02-16 23:11:33 +0000

    Added ejs module to server

--- express_server.js
+++ express_server.js
@@ -1,7 +1,8 @@
+//Require Modules and set up server
 const express = require('express');
-
 const app = express();
 const PORT = 8080;
+app.set('view engine', 'ejs');
 
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -12,6 +13,8 @@ app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
 });
 
+
+//Set Server to listen to port 8080
 app.listen( PORT, () => {
   console.log(`TinyApp Server listening on Port: ${PORT}`);
 });
