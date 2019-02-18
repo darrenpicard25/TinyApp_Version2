2ad21d3edf71de5513c68bfd74a0bd870674f6d1
Darren Picard <darrenpicard25@gmail.com>
2019-02-18 06:36:15 +0000

    All done. Files cleaned up and README.md file created

---
+++ README.md
@@ -0,0 +1,24 @@
+# TinyApp Project
+
+TinyApp is a full stack web application built with Node and Express that allows users to shorten long URLS.
+
+## Final Product
+
+!["Image of home page logged in"](https://github.com/darrenpicard25/tinyApp_Project/blob/master/docs/urls_home_loggedin.png?raw=true)
+!["Image of home page logged out"](https://github.com/darrenpicard25/tinyApp_Project/blob/master/docs/urls_home_logout.png?raw=true)
+!["Image of URL editing page"](https://github.com/darrenpicard25/tinyApp_Project/blob/master/docs/urls_edit.png?raw=true)
+
+## Dependencies
+
+- Node.js
+- Express
+- EJS
+- bcrypt
+- body-parser
+- cookie-session
+
+## Getting Started
+
+- Install all dependencies (using the npm install command
+- Run the develpoment web server using the node express_server.js command
+- make requests to port: 8080 (set default in file)
--- express_server.js
+++ express_server.js
@@ -1,10 +1,20 @@
-//Require Modules and set up server
+/*
+
+Require All Modules to be used
+----------------------------------------------------------
+*/
 const express = require('express');
 const cookieSession = require('cookie-session');
 const app = express();
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt');
 const PORT = 8080;
+
+/*
+
+Set Server to use certain modules
+------------------------------------------------------------
+*/
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieSession({
@@ -14,15 +24,19 @@ app.use(cookieSession({
   maxAge: 24 * 60 * 60 * 1000 // 24 hours
 }));
 
+/*
+
+All functions used in server
+----------------------------------------------------------------------------------
+*/
 function generateRandomString() {
   const characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789';
   let randID = '';
   for (let i = 1; i <= 6; i++) {
-    randID += characters[Math.floor(Math.random()*characters.length)];
+    randID += characters[Math.floor(Math.random() * characters.length)];
   }
   return randID;
 }
-
 function emailChecker(email) {
   for (let user in usersDatabase) {
     if (usersDatabase[user].email === email) {
@@ -31,9 +45,8 @@ function emailChecker(email) {
   }
   return '';
 }
-
 function urlsForUser (id) {
-  let userURLs = {};
+  const userURLs = {};
   for (let url in urlDatabase) {
     if (urlDatabase[url].userID === id) {
       userURLs[url] = urlDatabase[url];
@@ -41,48 +54,104 @@ function urlsForUser (id) {
   }
   return userURLs;
 }
+/*
 
+Both URL and User Databases
+
+*/
 const urlDatabase = {};
 
 const usersDatabase = {};
 
+/*
 
+All Unique Requests
+----------------------------------------------------
+*/
+app.get('/u/:shortURL', (req, res) => {
+  try {
+    const link = urlDatabase[req.params.shortURL].longURL;
+    res.redirect(link);
+  }
+  catch (error) {
+    res.send('This Short URL no longer Exists\n');
+  }
+});
 app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
 });
+/*
 
+All Main GET Requests
+-----------------------------------------------------
+*/
 app.get('/urls', (req, res) => {
-  let templateData = {
+  const templateData = {
     'urls' : urlsForUser(req.session.user_id),
     username: usersDatabase[req.session.user_id]
      };
   res.render('urls_index', templateData);
 });
-app.post('/urls', (req, res) => {
-  let newURL = req.body.longURL;
-  let userID = req.session.user_id;
-  let shortURL = generateRandomString();
-  urlDatabase[shortURL] = {longURL: newURL, userID: userID};
-  res.redirect(`/urls`);
 
-
+app.get('/register' , (req, res) => {
+  const templateData = {
+    'urls' : urlDatabase,
+    username: usersDatabase[req.session.user_id]
+   };
+  res.render('urls_register', templateData);
 });
-app.post('/urls/:shortURL/delete', (req, res) => {
-  let currentUser = req.session.user_id;
-  let urlCreator = urlDatabase[req.params.shortURL].userID;
-  if (currentUser === urlCreator) {
-    let deletingURL = req.params.shortURL;
-    delete urlDatabase[deletingURL];
-    res.redirect('/urls');
+
+app.get('/login', (req, res) => {
+  const templateData = {
+    'urls' : urlDatabase,
+    username: usersDatabase[req.session.user_id]
+    };
+  res.render('urls_login', templateData);
+});
+
+app.get("/urls/new", (req, res) => {
+  if(req.session.user_id) {
+    const templateData = {
+    'urls' : urlDatabase,
+    username: usersDatabase[req.session.user_id]
+     };
+    res.render("urls_new", templateData);
   } else {
-      res.send('This page does not belong to you. So you cannot delete it\n');
+    res.redirect('/login');
   }
 });
 
+app.get('/urls/:shortURL', (req, res) => {
+  const currentUser = req.session.user_id;
+  const urlCreator = urlDatabase[req.params.shortURL].userID;
+
+  if (currentUser === urlCreator) {
+    const templateData = {
+      shortURL: req.params.shortURL,
+      longURL: urlDatabase[req.params.shortURL].longURL,
+      username: usersDatabase[req.session.user_id]
+    };
+    res.render('urls_show', templateData);
+  } else {
+    res.send('This page does not belong to you\n');
+  }
+});
+/*
+
+All POST Requests
+----------------------------------------------------------------
+*/
+app.post('/urls', (req, res) => {
+  const newURL = req.body.longURL;
+  const userID = req.session.user_id;
+  const shortURL = generateRandomString();
+  urlDatabase[shortURL] = {longURL: newURL, userID: userID};
+  res.redirect(`/urls`);
+});
 app.post('/login', (req, res) => {
-  let email = req.body.email;
-  let password = req.body.password;
-  let id = emailChecker(email);
+  const email = req.body.email;
+  const password = req.body.password;
+  const id = emailChecker(email);
 
   if (id && bcrypt.compareSync(password ,usersDatabase[id].password)) {
     req.session.user_id = id;
@@ -92,33 +161,13 @@ app.post('/login', (req, res) => {
   }
 });
 
-app.post('/urls/:shortURL', (req, res) => {
-  let currentUser = req.session.user_id;
-  let urlCreator = urlDatabase[req.params.shortURL].userID;
-  if (currentUser === urlCreator) {
-    let newURL = req.body.newlongURL;
-    let shortURL = req.params.shortURL;
-    urlDatabase[shortURL].longURL = newURL;
-    res.redirect('/urls');
-  } else {
-      res.send('This page does not belong to you. So you cannot edit it\n');
-  }
-});
-
-app.get('/register' , (req, res) => {
-  let templateData = {
-    'urls' : urlDatabase,
-    username: usersDatabase[req.session.user_id]
-   };
-  res.render('urls_register', templateData);
-});
-
 app.post('/register', (req, res) => {
-  let newEmail = req.body.email;
-  let newPassword = req.body.password;
-  let newId = generateRandomString();
+  const newEmail = req.body.email;
+  const newPassword = req.body.password;
+  const newId = generateRandomString();
+
   if (newEmail && newPassword && !emailChecker(newEmail)) {
-    let hashedPassword = bcrypt.hashSync(newPassword, 10);
+    const hashedPassword = bcrypt.hashSync(newPassword, 10);
     usersDatabase[newId] = {
     id: newId,
     email: newEmail,
@@ -131,50 +180,41 @@ app.post('/register', (req, res) => {
   }
 });
 
-app.get('/login', (req, res) => {
-  let templateData = {
-    'urls' : urlDatabase,
-    username: usersDatabase[req.session.user_id]
-    };
-  res.render('urls_login', templateData);
-});
-
-app.get('/u/:shortURL', (req, res) => {
-  let link = urlDatabase[req.params.shortURL].longURL;
-  res.redirect(link);
-});
-
-app.get("/urls/new", (req, res) => {
-  let templateData = {
-  'urls' : urlDatabase,
-  username: usersDatabase[req.session.user_id]
-   };
-  res.render("urls_new", templateData);
-});
-
 app.post('/logout', (req, res) => {
   req.session.user_id = null;
   res.redirect('/urls');
 });
 
-
-app.get('/urls/:shortURL', (req, res) => {
-  let currentUser = req.session.user_id;
-  let urlCreator = urlDatabase[req.params.shortURL].userID;
+app.post('/urls/:shortURL/delete', (req, res) => {
+  const currentUser = req.session.user_id;
+  const urlCreator = urlDatabase[req.params.shortURL].userID;
   if (currentUser === urlCreator) {
-    let templateData = {
-      shortURL: req.params.shortURL,
-      longURL: urlDatabase[req.params.shortURL].longURL,
-      username: usersDatabase[req.session.user_id]
-    };
-    res.render('urls_show', templateData);
+    const deletingURL = req.params.shortURL;
+    delete urlDatabase[deletingURL];
+    res.redirect('/urls');
   } else {
-    res.send('This page does not belong to you\n');
+      res.send('This page does not belong to you. So you cannot delete it\n');
   }
 });
 
+app.post('/urls/:shortURL', (req, res) => {
+  const currentUser = req.session.user_id;
+  const urlCreator = urlDatabase[req.params.shortURL].userID;
+  if (currentUser === urlCreator) {
+    const newURL = req.body.newlongURL;
+    const shortURL = req.params.shortURL;
+    urlDatabase[shortURL].longURL = newURL;
+    res.redirect('/urls');
+  } else {
+      res.send('This page does not belong to you. So you cannot edit it\n');
+  }
+});
 
-//Set Server to listen to port 8080
+/*
+
+Let Server start listening for requests on the Port
+----------------------------------------------------------
+*/
 app.listen( PORT, () => {
   console.log(`TinyApp Server listening on Port: ${PORT}`);
 });
